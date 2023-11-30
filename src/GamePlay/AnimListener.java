62c77d4bd2807e16aa6878dff38cb0291e933de5
ahmedtaha-ux <ahmedtaha9792@gmail.com>
2023-12-01 00:32:42 +0200

    project structure and start code

---
+++ src/GamePlay/AnimListener.java
@@ -0,0 +1,13 @@
+
+package fish;
+
+import javax.media.opengl.GLEventListener;
+
+
+
+public abstract class AnimListener implements GLEventListener{
+ 
+    protected String assetsFolderName = "Assets//Fish";
+    
+}
+
---
+++ src/GamePlay/RiverRaid.java
@@ -0,0 +1,2 @@
+package GamePlay;public class RiverRaid {
+}
